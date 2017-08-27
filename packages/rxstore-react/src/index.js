0f1fc6757d5bf5f1b9cd06dd52050214bfa7e060
Ben Ripkens <bripkens@gmail.com>
2017-08-27 08:38:12 +0200

    get started with react connection

---
+++ packages/rxstore-react/src/index.js
@@ -0,0 +1,106 @@
+import {Component} from 'react';
+
+export function connectTo(createObservables, ComposedComponent) {
+  const needsToCreateObservablesWhenPropsChange = typeof createObservables === 'function';
+
+  return class extends Component {
+    static displayName = 'connectTo for ' + (ComposedComponent.displayName || ComposedComponent.name);
+
+    state = {};
+    subscriptions = new Map();
+    observables = new Map();
+
+    componentWillMount() {
+      let observables;
+      if (needsToCreateObservablesWhenPropsChange) {
+        observables = createObservables(this.props);
+      } else {
+        observables = createObservables;
+      }
+      this.subscribe(observables);
+    }
+
+    componentWillReceiveProps(nextProps) {
+      if (needsToCreateObservablesWhenPropsChange && !shallowEquals(this.props, nextProps)) {
+        this.subscribe(createObservables(nextProps));
+      }
+    }
+
+    subscribe(observables) {
+      const newProperties = Object.keys(observables);
+      const oldProperties = this.observables;
+
+      for (let i = 0, length = newProperties.length; i < length; i++) {
+        const property = newProperties[i];
+        const prevObservable = this.observables.get(property);
+        const newObservable = observables[property];
+
+        if (prevObservable === newObservable) {
+          // Nothing to do, we have the same observable
+          continue;
+        }
+
+        const oldSubscription = this.subscriptions.get(property);
+        this.observables.set(property, newObservable);
+        this.subscriptions.set(property, newObservable.subscribe(this.onNewValue.bind(null, property)));
+
+        // unsubscribe previous subscriptions only after new subscriptions were
+        // established. This makes reference counting more efficient for
+        // subscriptions which are immediately unsubscribed or for which
+        // values are immediately recalculated.
+        if (oldSubscription) {
+          oldSubscription.unsubscribe();
+        }
+      }
+
+      // Remove properties / subscriptions for all properties that haven't been
+      // recreated / are not found in the new observable map.
+      const removedProperties = [];
+      oldProperties.forEach((property, key) => {
+        if (!observables[key]) {
+          removedProperties.push(key);
+        }
+      });
+
+      const clearStateProps = {};
+      for (let i = 0, len = removedProperties.length; i < len; i++) {
+        const property = removedProperties[i];
+        this.subscriptions.get(property).unsubscribe();
+        this.subscriptions.delete(property);
+        this.observables.delete(property);
+        clearStateProps[property] = null;
+      }
+      this.setState(clearStateProps);
+    }
+
+    onNewValue = (value, property) => {
+      this.setState({
+        [property]: value
+      });
+    };
+
+    componentWillUnmount() {
+      this.subscriptions.forEach(sub => sub.unsubscribe());
+    }
+
+    render() {
+      return <ComposedComponent {...this.props} {...this.state} />;
+    }
+  };
+}
+
+function shallowEquals(a, b) {
+  const aKeys = Object.keys(a);
+  const bKeys = Object.keys(b);
+  if (aKeys.length !== bKeys.length) {
+    return false;
+  }
+
+  for (let key in a) {
+    if (a[key] !== b[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
---
+++ packages/rxstore-react/test/index.test.js
@@ -0,0 +1,5 @@
+/* eslint-env jest */
+
+test('placeholder', () => {
+
+});
